4db6309607e84e7a9512db536f0d70f2460dd4b1
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-06-26 19:42:08 +0530

    commit

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
     <div className="text-center p-4 bg-blue-100">
       <div>My Resume</div>
       <h1 className="text-3xl font-bold text-blue-800">
-        Hello, Tailwind + Vite!
+        Premendra Kumar
       </h1>
     </div>
   );
