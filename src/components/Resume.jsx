bf9a011ce655d6e340b8374a08ed755d2c99f926
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-06-27 15:44:46 +0530

    commit

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,18 +5,27 @@ const Resume = () => {
     <div className="text-center p-4 bg-blue-100">
       {/* <div>My Resume</div> */}
       {/* <h1 className="text-3xl font-bold text-blue-800">Premendra Kumar</h1> */}
-      <div className="p-4 border rounded-lg bg-gray-100">
-        <div className="flex gap-4">
-          <div className="flex-1 bg-blue-200 p-4 rounded text-left">
-            Premendra Kumar <br />
-            Senior Staff Engineer
-            </div>
-          <div className="flex-1 bg-green-200 p-4 rounded text-left">
-            <b>Phone: </b> +91 80106 45624, +91 87500 84750 <br />
-            <b>Email: </b> premendra.bce05515@gmail.com, premendra.bce05515@outlook.com <br />
-            <b>Total Experience: </b> 14 years 2 months
-          </div>
+
+      <div className="flex">
+        <div className="flex-1 rounded text-left">
+          Premendra Kumar <br />
+          Senior Staff Engineer
         </div>
+        <div className="flex-1 rounded text-left">
+          <b>Phone: </b> +91 80106 45624, +91 87500 84750 <br />
+          <b>Email: </b> premendra.bce05515@gmail.com,
+          premendra.bce05515@outlook.com <br />
+          <b>Total Experience: </b> 14 years 2 months
+        </div>
+      </div>
+      <div className="text-left">
+        Seeking a position to utilize my skills and abilities with approx. <b>14
+        years 3 months of experience</b> (approx. 14.3 years of backend development,
+        8.5 years of fullstack development, team lead and devops) in the
+        Information Technology Industry that offers professional growth while
+        being resourceful, innovative and flexible. I want to work with a
+        professional organization where I will be able to develop my skills
+        along with some challenging projects to handle with latest technology.
       </div>
     </div>
   );
